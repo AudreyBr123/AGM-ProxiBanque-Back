575fd415405c9ee0158c0d772040834d7eaa9196
marine-spaak <marine.spaak@gmail.com>
2024-05-15 10:26:31 +0200

    Initial commit

---
+++ src/main/java/org/formation/AgmProxiBanqueBackApplication.java
@@ -0,0 +1,13 @@
+package org.formation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgmProxiBanqueBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgmProxiBanqueBackApplication.class, args);
+	}
+
+}
