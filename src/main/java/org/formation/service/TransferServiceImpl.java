834a48be817b868d938587b74cd641a656d08125
AudreyBr123 <boureau.audrey@gmail.com>
2024-05-24 16:03:37 +0200

    AP-26-feat :sparkles: transfer between all types of accounts (except saving to saving) works

--- src/main/java/org/formation/service/TransferServiceImpl.java
+++ src/main/java/org/formation/service/TransferServiceImpl.java
@@ -13,36 +13,100 @@ public class TransferServiceImpl implements TransferService {
 	CurrentAccountRepository currentAccountRepository;
 	SavingAccountRepository savingAccountRepository;
 
+	//Constructeur
 	public TransferServiceImpl(CurrentAccountRepository currentAccountRepository,
 			SavingAccountRepository savingAccountRepository) {
 		this.currentAccountRepository = currentAccountRepository;
 		this.savingAccountRepository = savingAccountRepository;
 	}
 
+	/**
+	 * Méthode publique pour executer un virement
+	 * @param transfer
+	 */
 	public void executeTransfer(Transfer transfer) {
 
+		if (transfer.getTypeCreditAccount().equals("currentAccount")
+				&& transfer.getTypeDebitAccount().equals("savingAccount")) {
+			initiateTransferFromSavingToCurrent(transfer);
+		}
+		if (transfer.getTypeCreditAccount().equals("currentAccount")
+				&& transfer.getTypeDebitAccount().equals("currentAccount")) {
+			initiateTransferFromCurrentToCurrent(transfer);
+		}
+		if (transfer.getTypeCreditAccount().equals("savingAccount")
+				&& transfer.getTypeDebitAccount().equals("currentAccount")) {
+			initiateTransferFromCurrentToSaving(transfer);
+		}
+	}
+
+	/**
+	 * Méthode privée pour executer un virement d'un compte épargne vers un compte courant
+	 * @param transfer
+	 */
+	private void initiateTransferFromSavingToCurrent(Transfer transfer) {
 		// trouve le compte de crédit (courant)
-		if (transfer.getTypeCreditAccount().equals("currentAccount")) {
-			CurrentAccount creditAccount = currentAccountRepository.findById(transfer.getIdCreditAccount())
-					.orElse(null);
+		CurrentAccount creditAccount = currentAccountRepository.findById(transfer.getIdCreditAccount()).orElse(null);
 
-			// trouve le compte de débit (épargne)
-			if (transfer.getTypeDebitAccount().equals("savingAccount")) {
-				SavingAccount debitAccount = savingAccountRepository.findById(transfer.getIdDebitAccount())
-						.orElse(null);
+		// trouve le compte de débit (épargne)
+		SavingAccount debitAccount = savingAccountRepository.findById(transfer.getIdDebitAccount()).orElse(null);
 
-				// si les deux ne sont pas null, executer le virement
-				if (debitAccount != null && creditAccount != null) {
-					debitAccount.debitAmount(transfer.getAmount());
-					creditAccount.creditAmount(transfer.getAmount());
-				}
-
-				// persister les deux
-				currentAccountRepository.save(creditAccount);
-				savingAccountRepository.save(debitAccount);
-			}
+		// si les deux ne sont pas null, executer le virement
+		if (debitAccount != null && creditAccount != null) {
+			debitAccount.debitAmount(transfer.getAmount());
+			creditAccount.creditAmount(transfer.getAmount());
 		}
 
+		// persister les deux
+		currentAccountRepository.save(creditAccount);
+		savingAccountRepository.save(debitAccount);
 	}
+	
+	
+	/**
+	 * Méthode privée pour executer un virement d'un compte courant vers un compte courant
+	 * @param transfer
+	 */
+	private void initiateTransferFromCurrentToCurrent(Transfer transfer) {
+		// trouve le compte de crédit (courant)
+		CurrentAccount creditAccount = currentAccountRepository.findById(transfer.getIdCreditAccount()).orElse(null);
+
+		// trouve le compte de débit (courant)
+		CurrentAccount debitAccount = currentAccountRepository.findById(transfer.getIdDebitAccount()).orElse(null);
+
+		// si les deux ne sont pas null, executer le virement
+		if (debitAccount != null && creditAccount != null) {
+			debitAccount.debitAmount(transfer.getAmount());
+			creditAccount.creditAmount(transfer.getAmount());
+		}
+
+		// persister les deux
+		currentAccountRepository.save(creditAccount);
+		currentAccountRepository.save(debitAccount);
+	}
+	
+	/**
+	 * Méthode privée pour executer un virement d'un compte courant vers un compte épargne
+	 * @param transfer
+	 */
+	private void initiateTransferFromCurrentToSaving(Transfer transfer) {
+		// trouve le compte de crédit (courant)
+		SavingAccount creditAccount = savingAccountRepository.findById(transfer.getIdCreditAccount()).orElse(null);
+
+		// trouve le compte de débit (épargne)
+		CurrentAccount debitAccount = currentAccountRepository.findById(transfer.getIdDebitAccount()).orElse(null);
+
+		// si les deux ne sont pas null, executer le virement
+		if (debitAccount != null && creditAccount != null) {
+			debitAccount.debitAmount(transfer.getAmount());
+			creditAccount.creditAmount(transfer.getAmount());
+		}
+
+		// persister les deux
+		savingAccountRepository.save(creditAccount);
+		currentAccountRepository.save(debitAccount);
+	}
+	
+	//Impossible de faire un virement d'un compte épargne vers un autre compte épargne
 
 }
