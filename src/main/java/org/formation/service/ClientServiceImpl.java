e5369feb99a45771a54eb40935233a6764c377ef
marine-spaak <marine.spaak@gmail.com>
2024-06-03 17:04:56 +0200

    AP-40 chore :art: init project with clean dataset

--- src/main/java/org/formation/service/ClientServiceImpl.java
+++ src/main/java/org/formation/service/ClientServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ClientServiceImpl implements ClientService {
-//	private List<Client> clients = new ArrayList<>();
 	private ClientRepository repository;
 	private AdvisorRepository advisorRepository;
 	
@@ -19,25 +18,10 @@ public class ClientServiceImpl implements ClientService {
 		this.repository = repository;
 		this.advisorRepository = advisorRepository;
 	}
-	
-//	public ClientServiceImpl() {
-//		
-//		Address genericAddress = new Address("rue de la paix", "75000", "Paris");
-//		CurrentAccount genericCurrentAccount = new CurrentAccount(122.2);
-//		SavingAccount genericSavingAccount = new SavingAccount(122.2);
-//
-//		
-//		clients.addAll(List.of(
-//				new Client("Audrey", "Boureau", "a.boureau@gmail.com", "0000000", genericAddress, genericCurrentAccount, genericSavingAccount),
-//				new Client("Gwendal", "Breton", "g.breton@gmail.com", "0000000", genericAddress, genericCurrentAccount, genericSavingAccount),
-//				new Client("Marine", "Spaak", "m.spaak@gmail.com", "0000000", genericAddress, genericCurrentAccount, genericSavingAccount)
-//				));
-//		}
 
 	@Override
 	public List<Client> getAll() {
 		return repository.findAll();
-	//	return clients;
 	}
 
 	@Override
