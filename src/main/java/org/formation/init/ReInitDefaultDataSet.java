47d3e463f522d3e6fac09db975604937a5107ccd
marine-spaak <marine.spaak@gmail.com>
2024-06-10 15:05:04 +0200

    AP-60 chore :art: add clients to reinit dataset

--- src/main/java/org/formation/init/ReInitDefaultDataSet.java
+++ src/main/java/org/formation/init/ReInitDefaultDataSet.java
@@ -41,14 +41,50 @@ public class ReInitDefaultDataSet {
 		CurrentAccount thirdCurrentAccount = new CurrentAccount(655.5);
 		SavingAccount thirdSavingAccount = new SavingAccount(1000);
 		
+		PersonInfos fourthInfos = new PersonInfos("Alexane",	"Durozier", "alexane.durozier@dev.fr", "0608080808", "rue du Pilou-pilou", "31000", "Toulouse");
+		CurrentAccount fourthCurrentAccount = new CurrentAccount(103);
+		SavingAccount fourthSavingAccount = new SavingAccount(3800);
+		
+		PersonInfos fifthInfos = new PersonInfos("Chloé",	"Sirgent", "cloclo@msn.com", "0608080808", "rue de la Soif", "44000", "Nantes");
+		CurrentAccount fifthCurrentAccount = new CurrentAccount(103);
+		SavingAccount fifthSavingAccount = new SavingAccount(3800);
+		
+		PersonInfos sixthInfos = new PersonInfos("Morgan",	"Durozier", "momo.durozier@dev.fr", "0608080808", "avenue de la Gymnastique", "75000", "Paris");
+		CurrentAccount sixthCurrentAccount = new CurrentAccount(903);
+		SavingAccount sixthSavingAccount = new SavingAccount(3200);
+		
+		PersonInfos seventhInfos = new PersonInfos("Elias",	"Asri", "elias.asri@motocross.com", "0608080808", "boulevard de la Vitesse", "31000", "Toulouse");
+		CurrentAccount seventhCurrentAccount = new CurrentAccount(93.8);
+		SavingAccount seventhSavingAccount = new SavingAccount(4150);
+		
+		PersonInfos eigthInfos = new PersonInfos("Marine",	"Delanoe", "marine.delanoe@dev.fr", "0608080808", "rue de l'Humour", "49000", "Angers");
+		CurrentAccount eigthCurrentAccount = new CurrentAccount(505.5);
+		SavingAccount eigthSavingAccount = new SavingAccount(2505);
+		
+		PersonInfos ninthInfos = new PersonInfos("Ameline",	"Houis", "ameline.houis@capgemini.fr", "0608080808", "rue du Chaton", "44000", "Nantes");
+		CurrentAccount ninthCurrentAccount = new CurrentAccount(109);
+		SavingAccount ninthSavingAccount = new SavingAccount(998);
+		
 		Client firstClient = new Client(firstInfos, firstCurrentAccount, firstSavingAccount);
 		Client secondClient = new Client(secondInfos, secondCurrentAccount, secondSavingAccount);
 		Client thirdClient = new Client(thirdInfos, thirdCurrentAccount, thirdSavingAccount);
+		Client fourthClient = new Client(fourthInfos, fourthCurrentAccount, fourthSavingAccount);
+		Client fifthClient = new Client(fifthInfos, fifthCurrentAccount, fifthSavingAccount);
+		Client sixthClient = new Client(sixthInfos, sixthCurrentAccount, sixthSavingAccount);
+		Client seventhClient = new Client(seventhInfos, seventhCurrentAccount, seventhSavingAccount);
+		Client eigthClient = new Client(eigthInfos, eigthCurrentAccount, eigthSavingAccount);
+		Client ninthClient = new Client(ninthInfos, ninthCurrentAccount, ninthSavingAccount);
 
 		clientService.save(firstClient);
 		clientService.save(secondClient);
 		clientService.save(thirdClient);
-	
+		clientService.save(fourthClient);
+		clientService.save(fifthClient);
+		clientService.save(sixthClient);
+		clientService.save(seventhClient);
+		clientService.save(eigthClient);
+		clientService.save(ninthClient);
+
 		// Conseillers
 		PersonInfos firstAdvisorInfos = new PersonInfos("Muriel",	"Lebourdais", "m.lebourdais@m2i.fr", "0606060606", "rue de la Formation", "75000", "Paris");
 		PersonInfos secondAdvisorInfos = new PersonInfos("Yann",	"Creach", "y.creach@m2i.fr", "0607080910", "boulevard de la Pédagogie", "75000", "Paris");
@@ -62,7 +98,14 @@ public class ReInitDefaultDataSet {
 		// Affectation des clients aux conseillers
 		clientService.assignAdvisorToClient(firstClient.getId(), secondAdvisor.getId());
 		clientService.assignAdvisorToClient(secondClient.getId(), secondAdvisor.getId());
-		clientService.assignAdvisorToClient(thirdClient.getId(), firstAdvisor.getId());
+		clientService.assignAdvisorToClient(thirdClient.getId(), secondAdvisor.getId());
+		clientService.assignAdvisorToClient(fourthClient.getId(), secondAdvisor.getId());
+		clientService.assignAdvisorToClient(fifthClient.getId(), secondAdvisor.getId());
+		clientService.assignAdvisorToClient(sixthClient.getId(), firstAdvisor.getId());
+		clientService.assignAdvisorToClient(seventhClient.getId(), firstAdvisor.getId());
+		clientService.assignAdvisorToClient(eigthClient.getId(), firstAdvisor.getId());
+		clientService.assignAdvisorToClient(ninthClient.getId(), firstAdvisor.getId());
+
 	}
 
 }
